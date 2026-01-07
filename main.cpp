fe39e25793de25d905bad215ff1853eefca7804c
Bether12 <elnenogomez13@gmail.com>
2026-01-07 12:36:21 -0500

    Add conditional block to test positional argument entry

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 
-int main(){
+int main(int argc, char* argv[]){
+    if(argc>2){
+        std::cout<<argv[1]<<' '<<argv[2];
+    }else{
+        std::cout<<"Enter at least 2 commands, or read the README.md"<<std::endl;
+    }
     return 0;
 }
