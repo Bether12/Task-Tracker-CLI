de60af22c8d8eca0a575e0dd43f4c531fe5b61e1
Bether12 <elnenogomez13@gmail.com>
2026-01-07 14:14:49 -0500

    Create task.h with the Task struct for grouping all the task data in one
    There is no need for a class declaration since is
    desired a public access modifier and no need for
    methods

---
+++ include/task.h
@@ -0,0 +1,14 @@
+#ifndef TASK_TASK_H_
+#define TASK_TASK_H_
+#include<string>
+
+struct Task
+{
+    int id;
+    std::string description;
+    std::string status;
+    std::string createdAt;
+    std::string createdAt;
+};
+
+#endif TASK_TASK_H_
